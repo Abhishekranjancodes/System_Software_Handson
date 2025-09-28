014864ef1023f5f9e37dd826ebf7639fe2d4e775
Abhishek Ranjan <abhi7250250@gmail.com>
2025-09-28 14:05:12 +0530

    question 14 done

--- handson_2/14.c
+++ handson_2/14.c
@@ -1,7 +1,19 @@
+/*
+============================================================================
+Name: 14.c
+Author: Abhishek Ranjan
+Description: Program to create a pipe,write to the pipe, read from pipe and
+		display on the monitor.
+
+Date: 28th sept, 2025
+============================================================================
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 
 int main(){
+
 	int fd[2];
 	pipe(fd);
 	char buf[100];
@@ -9,3 +21,15 @@ int main(){
 	read(fd[0],buf,bytes);
 	printf("message:%s\n",buf);
 }
+
+/*
+===============================================================================
+Output:
+
+oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$ cc 14.c
+oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$ ./a.out
+message:Abhishek_Ranjan
+oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$
+
+===============================================================================
+*/
