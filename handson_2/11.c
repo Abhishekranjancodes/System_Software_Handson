bae24588e53b8cf003e8af36fc349aad1c08e50e
Abhishek Ranjan <abhi7250250@gmail.com>
2025-09-28 11:20:26 +0530

    question 11 done

--- handson_2/11.c
+++ handson_2/11.c
@@ -1,9 +1,9 @@
 /*
 ============================================================================
-Name: 9.c
+Name: 11.c
 Author: Abhishek Ranjan
 Description: Program to ignore a SIGINT signal then reset the default action 
-		of the SIGINT signal - using signal system call
+		of the SIGINT signal - using sigaction system call
 
 Date: 28th sept, 2025
 ============================================================================
@@ -14,27 +14,31 @@ Date: 28th sept, 2025
 #include <unistd.h>
 
 int main() {
-    signal(SIGINT, SIG_IGN);
-    printf("SIGINT is now getting ignored for the next 7 second\n");
+	struct sigaction sa;
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+    	printf("SIGINT is now getting ignored for the next 7 second\n");
     
-    sleep(7);
+    	sleep(7);
+	
+	sa.sa_handler = SIG_DFL;
+    	sigaction(SIGINT, &sa, NULL);
+    	printf("SIGINT is now reset to default, An infinite loop is running press CTRL+C to exit\n");
 
-    signal(SIGINT, SIG_DFL);
-    printf("SIGINT is now reset to default, An infinite loop is running press CTRL+C to exit\n");
+    	while (1);
 
-    while (1);
-
-    return 0;
+    	return 0;
 }
 
 /*
 ===============================================================================
 Output:
 
-oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$ cc 9.c
+oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$ cc 11.c
 oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$ ./a.out
 SIGINT is now getting ignored for the next 7 second
-^C^C^C^C^C^C^CSIGINT is now reset to default, An infinite loop is running press CTRL+C to exit
+^C^C^C^C^C^C^C^C^CSIGINT is now reset to default, An infinite loop is running press CTRL+C to exit
 ^C
 oldfish@oldfish-Katana-GF66-12UC:~/Desktop/Term_1/Linux_call/handson/handson_2$
 
